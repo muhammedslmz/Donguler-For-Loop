1653c4f8127114f9947f985e86bba0c633632d67
Muhammed Solmaz <muhammed.slmz@gmail.com>
2022-08-08 16:35:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Donguler_for_loop
+{
+    class Program
+    {
+        static void Main (string[]args)
+        {
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi= Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= sayi; i++)
+            {
+                if (i%2==1)
+                Console.WriteLine(i);
+            }
+
+            int tektoplam=0;
+            int cifttoplam=0;  
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if(i%2 == 1)
+                tektoplam += i;
+
+                else 
+                cifttoplam +=i;
+            }
+            Console.WriteLine("Tek sayılar toplamı: "+tektoplam);
+            Console.WriteLine("Çift sayıların toplamı: "+cifttoplam);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i==4)
+                break;
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+             if(i==4)
+             continue;
+             Console.WriteLine(i);   
+            }
+        }
+    }
+}
